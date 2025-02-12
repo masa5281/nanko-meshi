9de7592a3c449fe4b752d6cc97f4229fb92b17a5
masa5281 <masa.h.5281@gmail.com>
2025-02-12 18:03:29 +0900

    chore: Tailwind CSSの適用対象のファイルを指定（#4）
    - `tailwind.config.js`のcontentセクションを編集して、スタイルを適用するファイルを指定
    - `index.css`にTailwind CSSのクラスをCSSのプロパティに変更する記述を追記

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx}", // すべてのJS、JSXファイルをスキャン
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
