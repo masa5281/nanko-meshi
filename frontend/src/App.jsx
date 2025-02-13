82c0d716121991f34806d05f23c3452037557c89
masa5281 <masa.h.5281@gmail.com>
2025-02-13 17:55:03 +0900

    feat: 消費カロリー入力処理のマークアップを実装（#5）

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
-const App = () => {
+export const App = () => {
   return (
-    <h1>あああ</h1>
+    <>
+      <h2>消費したカロリーを入力!</h2>
+      <p>運動で消費したカロリーを食べ物の個数に換算できます！</p>
+      <div>
+        <input type="number" placeholder="カロリーを入力してください" />
+        <button>食べ物に換算</button>
+      </div>
+    </>
   );
 }
 
