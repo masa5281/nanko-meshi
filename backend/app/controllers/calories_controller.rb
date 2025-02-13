f3f33e055a9a21f3e421117c0ddce8bfbcc9bbd7
masa5281 <masa.h.5281@gmail.com>
2025-02-14 08:56:05 +0900

    feat: 消費カロリー入力機能の保存処理をAPI経由で実装（#5）
    - Caloriesコントローラーにcreateアクション、ストロングパラメーター`calorie_params`を実装
    - App.jsxにaxiosを用いたRailsへのPOST処理を実装

--- backend/app/controllers/calories_controller.rb
+++ backend/app/controllers/calories_controller.rb
@@ -1,2 +1,18 @@
 class CaloriesController < ApplicationController
+  def create
+    calorie = Calorie.new(calorie_params)
+
+    if calorie.save
+      render json: { status: 200, calorie: calorie }
+    else
+      render json: { status: 500, message: "カロリーの入力に失敗しました" }
+    end
+  end
+  
+  private
+  
+  # フロントから送られてくるデータ
+  def calorie_params
+    params.require(:calorie).permit(:burned_calorie, :recorded_at)
+  end
 end
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const App = () => {
+  const [calorieNum, setCalorieNum] = useState("");
+
+  const createBurnedCalorie = () => {
+    axios.post("http://localhost:3000/calories", {
+      burned_calorie: calorieNum
+    })
+    .then(() => {
+      setCalorieNum("");
+      console.log("OK");
+    })
+    // エラー時にアラートを表示
+    .catch((error) => {
+      window.alert(error.message)
+      console.log("NG");
+    })
+  };
+
   return (
     <>
       <h2>消費したカロリーを入力!</h2>
       <p>運動で消費したカロリーを食べ物の個数に換算できます！</p>
       <div>
-        <input type="number" placeholder="カロリーを入力してください" />
-        <button>食べ物に換算</button>
+        <input type="number" placeholder="カロリーを入力してください" value={calorieNum} onChange={(e) => setCalorieNum(Number(e.target.value))} />
+        <button onClick={createBurnedCalorie}>食べ物に換算</button>
       </div>
     </>
   );
