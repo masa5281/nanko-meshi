0111a438d89adbd04b9fc42477d59eb04d0dcd77
masa5281 <masa.h.5281@gmail.com>
2025-02-15 14:53:08 +0900

    feat: 登録日入力フォームのバリデーションの追加（#5）
    - 未来の日付を指定できないカスタムバリデーションを追加

--- backend/app/models/calorie.rb
+++ backend/app/models/calorie.rb
@@ -1,3 +1,11 @@
 class Calorie < ApplicationRecord
   validates :burned_calorie, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :recorded_at, presence: true
+  validate :validate_not_future_day
+
+  private
+
+  def validate_not_future_day
+    errors.add(:recorded_at, "には未来の日付を指定できません") if recorded_at.present? && recorded_at > Date.today
+  end
 end
