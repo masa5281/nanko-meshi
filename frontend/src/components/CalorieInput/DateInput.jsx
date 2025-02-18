69b08c756e23858e8d430303cd7905cecc37ba46
masa5281 <masa.h.5281@gmail.com>
2025-02-18 17:41:58 +0900

    refactor: カロリー入力フォームの日付入力をコンポーネント化（#5）

---
+++ frontend/src/components/CalorieInput/DateInput.jsx
@@ -0,0 +1,21 @@
+// アイコン用コンポーネント
+import { FaCalendarDays } from "react-icons/fa6";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// ロケールの変更
+import { registerLocale } from  "react-datepicker";
+import { ja } from 'date-fns/locale/ja';
+
+export const DateInput = (props) => {
+  // React Datepcikerの表示を日本語化
+  registerLocale('ja', ja);
+  
+  const { recordedDate, setRecordedDate } = props;
+  return (
+    <>
+      <label htmlFor="calendar" className="flex items-center pl-3 font-bold"><FaCalendarDays className="mr-0.5 text-lg" />日付</label>
+      <DatePicker id="calendar" className="indent-2 p-3 border-slate-900 border-2 rounded-full focus:ring-2 focus:ring-primary focus:border-primary focus:outline focus:outline-primary" dateFormat="yyyy/MM/dd" locale="ja" selected={recordedDate} onChange={(date) => setRecordedDate(date)} />
+    </>
+  );
+}
