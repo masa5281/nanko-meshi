ee6340342e18fbfa4be20817fd7666542abe9e09
masa5281 <masa.h.5281@gmail.com>
2025-02-27 06:06:02 +0900

    feat: createアクションのルーティングを設定（#10）

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "users/create"
+      resources :users, only: %i[create]
       resources :foods, only: %i[index]
       resources :calories, only: %i[create]
     end
