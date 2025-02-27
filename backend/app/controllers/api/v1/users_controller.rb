b74f43c0f7aca2c3cab6e13cdaf82f48677e4cef
masa5281 <masa.h.5281@gmail.com>
2025-02-27 17:25:56 +0900

    feat: `firebase_uid`と`name`カラムにデータ保存処理を実装（#10）
    - createUserApi関数内でuidを取得
    - nameカラムの簡易フォームを作成しステート変数に保存

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,18 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    
+    binding.pry
+    if user.save
+      render json: { status: 200, user: user }
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firebase_uid, :name)
   end
 end
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,26 +1,40 @@
 import { useState } from "react";
 import { handleSignUp } from "../firebase/firebase";
+import { api } from "../api";
 
 export const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
 
   /**
    * JSDocで変数eの型を明記
    * @param {Event} e 
    */
-  const signUp = async (e) => {
+
+  const createUserApi = async (e) => {
     e.preventDefault();
-    const user = await handleSignUp(email, password);
-    console.log("User情報:", user);
-  };
+    const userCredential = await handleSignUp(email, password);
+    const uid = await userCredential.user.uid;
+
+    try {
+      await api.post("/api/v1/users", {
+        firebase_uid: uid,
+        name: userName
+      });
+      console.log("OK");
+    } catch (error) {
+      console.log("失敗");
+    }
+  }
 
   return (
     <div>
       <h2>新規登録</h2>
-      <form onSubmit={signUp}>
+      <form onSubmit={createUserApi}>
         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
         <button type="submit">新規登録</button>
       </form>
     </div>
