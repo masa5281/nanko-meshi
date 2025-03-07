80d38001d23969601cab17e206533935564472d4
masa5281 <masa.h.5281@gmail.com>
2025-03-08 06:57:32 +0900

    feat: UserモデルにAvatarUploaderを紐づけ（#10）

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+
   validates :firebase_uid, presence: true, uniqueness: true
   validates :name, presence:, uniqueness: true, length: { maximum:20 }
 end
