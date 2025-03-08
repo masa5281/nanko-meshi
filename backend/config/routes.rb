edc6ad083246ae348f8c5f4555a92c2cacc49793
masa5281 <masa.h.5281@gmail.com>
2025-03-08 15:25:40 +0900

    feat: firebase_uidをURLパラメータとして定義（#10）

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[create]
+      resources :users, param: :firebase_uid, only: %i[show create]
       resources :foods, only: %i[index]
       resources :calories, only: %i[create]
     end
