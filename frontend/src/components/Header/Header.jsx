26645c36875c43a680dad220b6aa0d91c7eeb594
masa5281 <masa.h.5281@gmail.com>
2025-03-09 08:16:11 +0900

    feat: 未ログイン時にヘッダーにログインボタンを表示（#10）

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -14,9 +14,11 @@ import food from "../../images/food-white.png"
 import graph from "../../images/graph-white.png"
 import logo from "../../images/logo.png"
 // アイコン
-import { PiSignOutFill } from "react-icons/pi";
+import { PiSignOutBold } from "react-icons/pi";
+import { PiSignInBold } from "react-icons/pi";
 // flowbite-reactのカスタムテーマ
 import { customTheme } from "../../theme/theme";
+import { IconContext } from "react-icons/lib";
 
 export const Header = () => {
   const [user] = useAuthState(auth);
@@ -41,26 +43,34 @@ export const Header = () => {
           <img src={logo} alt="ロゴ" className="w-full" />
         </div>
       </a>
-      { user &&
-      <nav className="flex items-center h-full">
-        <ul className="flex items-center h-full mr-4">
-          <IconList link={"/calorie/input"} img={fire} alt={"カロリー入力"} menuName={"カロリー入力"} />
-          <IconList img={food} alt={"食品登録"} menuName={"食品登録"} />
-          <IconList img={graph} alt={"総消費カロリー"} menuName={"総消費カロリー"} />
-        </ul>
-        <Flowbite theme={{ theme: customTheme }}>
-          <Dropdown label={
-              <img src={userImage} alt="" className="max-w-full max-h-full"/>
-            }
-            arrowIcon={false}
-            inline={true}
-          >
-            <Dropdown.Item icon={PiSignOutFill}>
-              <Link to={"/sign_in"} onClick={handleSignOut}>ログアウト</Link>
-            </Dropdown.Item>
-          </Dropdown>
-        </Flowbite>
-      </nav>
+      {user ? (
+        <nav className="flex items-center h-full">
+          <ul className="flex items-center h-full mr-4">
+            <IconList link={"/calorie/input"} img={fire} alt={"カロリー入力"} menuName={"カロリー入力"} />
+            <IconList img={food} alt={"食品登録"} menuName={"食品登録"} />
+            <IconList img={graph} alt={"総消費カロリー"} menuName={"総消費カロリー"} />
+          </ul>
+          <Flowbite theme={{ theme: customTheme }}>
+            <Dropdown label={
+                <img src={userImage} alt="" className="max-w-full max-h-full"/>
+              }
+              arrowIcon={false}
+              inline={true}
+            >
+              <Dropdown.Item icon={PiSignOutBold}>
+                <Link to={"/sign_in"} onClick={handleSignOut}>ログアウト</Link>
+              </Dropdown.Item>
+            </Dropdown>
+          </Flowbite>
+        </nav>
+      ) : (
+        <Link to={"/sign_in"} className="flex flex-col items-center text-white font-bold hover:text-hoverWhite">
+          <IconContext.Provider value={{ size: 26 }}>
+            <PiSignInBold />
+          </IconContext.Provider>
+          ログイン
+        </Link>
+      )
       }
     </header>
   );
