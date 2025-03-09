95ed69fe0b5392bf0215d1d8c37f921664fa9821
masa5281 <masa.h.5281@gmail.com>
2025-03-09 17:59:29 +0900

    refactor: PasswordResetFormを既存のInputFieldコンポーネントで置き換え（#10）

--- frontend/src/components/PasswordResetForm.jsx
+++ frontend/src/components/PasswordResetForm.jsx
@@ -1,26 +1,21 @@
 // コンポーネント
 import { auth } from "../firebase/firebase"
+import { InputField } from "./InputField";
 // ライブラリ
 import { sendPasswordResetEmail } from "firebase/auth"
-import { IconContext } from "react-icons/lib";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message"
+import { FormProvider, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 // アイコン
 import { IoMail } from "react-icons/io5";
 
 export const PasswordResetForm = () => {
   const navigate = useNavigate();
-
-  const {
-      register,
-      watch,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      mode: "onBlur",
-      criteriaMode: "all"
-    });
+  
+  const methods = useForm({
+    mode: "onBlur",
+    criteriaMode: "all"
+  });
+  const { watch, handleSubmit } = methods;
 
   const handlePasswordReset = () => {
     sendPasswordResetEmail(auth, watch("email"));
@@ -39,32 +34,22 @@ export const PasswordResetForm = () => {
           </p>
 
           <form onSubmit={handleSubmit(handlePasswordReset)}>
-            <div className="relative">
-              <input
+            <FormProvider {...methods}>
+              <InputField
                 type="email"
                 placeholder="メールアドレス"
-                className="w-full py-2 border-none rounded-full indent-8 focus:ring-2 focus:ring-secondary focus:border-secondary"
-                {...register("email", {
+                fieldName="email"
+                validationRule={{
                   required: "メールアドレスを入力してください",
                   pattern: {
                     value: /^[a-zA-Z0-9_.-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
                     message: "有効なメールアドレスを入力してください"
                   }
-                })}
+                }}
+                iconComponent={<IoMail />}
               />
-              <div className="absolute top-1 left-1 p-1 rounded-full bg-text">
-                <IconContext.Provider value={{ size: 24, color: "white" }}>
-                  <IoMail />
-                </IconContext.Provider>
-              </div>
-            </div>
-            <ErrorMessage
-              errors={errors}
-              name="email"
-              render={({ message }) => message ? (<p className="text-errorYellow text-sm">{message}</p>) : null}
-            />
-
-            <button type="submit" className="inline-block w-full mt-4 py-1 border-2 border-white rounded-full bg-primary text-white text-xl hover:bg-hover">
+            </FormProvider>
+            <button type="submit" className="inline-block w-full py-1 border-2 border-white rounded-full bg-primary text-white text-xl hover:bg-hover">
               パスワード再設定メールを送信
             </button>
           </form>
