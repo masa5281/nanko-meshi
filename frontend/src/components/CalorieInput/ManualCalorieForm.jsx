9e1d8d555c902ca387279372cf249112f34b8a42
masa5281 <masa.h.5281@gmail.com>
2025-03-14 15:06:57 +0900

    refactor: カロリー入力画面のコードを整理
    - SubmitButtonのコンポーネントに合わせて記述を修正
    - フォームのpaddignを削除し、高さを調整

--- frontend/src/components/CalorieInput/ManualCalorieForm.jsx
+++ frontend/src/components/CalorieInput/ManualCalorieForm.jsx
@@ -5,10 +5,11 @@ import { getUserApi } from "../../api/userApi";
 // コンポーネント
 import { DateInput } from "./DateInput";
 import { CalorieInputError } from "./CalorieInputError";
-import { CalorieSubmit } from "./CalorieSubmit";
+import { SubmitButton } from "../Button/SubmitButton"
 // ライブラリ
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 // アイコン
 import { FaFire } from "react-icons/fa6";
 // カスタムフック
@@ -20,6 +21,7 @@ export const ManualCalorieForm = () => {
   const [validateErrors, setValidateErrors] = useState([]);
   const navigate = useNavigate();
   const { user } = useAuth();
+  const { handleSubmit } = useForm();
 
   const createManualCalorie = async () => {
     try {
@@ -46,30 +48,25 @@ export const ManualCalorieForm = () => {
   }
 
   return (
-    <div className="relative max-w-lg mx-auto pt-12 pb-10 border-slate-900 border-4 rounded-md before:content-[''] before:absolute before:w-56 before:h-10 before:bg-background before:inline-block before:-top-5 before:left-1/2 before:-translate-x-1/2">
-      <p className="absolute -top-7 left-1/2 -translate-x-1/2 w-52 mx-auto mb-5 py-3 rounded-full bg-text text-white font-bold text-lg">直接入力する</p>
-
-      <div className="mb-6">
-        <div className="flex justify-center px-3">
-          <div className="flex flex-col items-star">
-            <label htmlFor="calorie" className="flex items-center pl-3 font-bold"><FaFire className="mr-0.5 text-lg" />消費カロリー（kcal）</label>
-            <input
-              type="text"
-              id="calorie"
-              className="mr-3 p-3 border-slate-900 border-2 rounded-full indent-2 focus:ring-2 focus:ring-primary focus:border-primary"
-              placeholder="例：300"
-              value={calorieNum}
-              onChange={onChangeToText} />
-            <CalorieInputError errors={validateErrors} column="burned_calorie" />
-          </div>
-          <div className="flex flex-col items-start">
-            <DateInput recordedDate={recordedDate} setRecordedDate={setRecordedDate} />
-            <CalorieInputError errors={validateErrors} column="recorded_at" />
-          </div>
+    <form onSubmit={handleSubmit(createManualCalorie)}>
+      <div className="flex justify-center px-3 mb-6">
+        <div className="flex flex-col items-star">
+          <label htmlFor="calorie" className="flex items-center pl-3 font-bold"><FaFire className="mr-0.5 text-lg" />消費カロリー（kcal）</label>
+          <input
+            type="text"
+            id="calorie"
+            className="mr-3 border-slate-900 border-2 rounded-full indent-2 focus:ring-2 focus:ring-primary focus:border-primary"
+            placeholder="例：300"
+            value={calorieNum}
+            onChange={onChangeToText} />
+          <CalorieInputError errors={validateErrors} column="burned_calorie" />
+        </div>
+        <div className="flex flex-col items-start">
+          <DateInput recordedDate={recordedDate} setRecordedDate={setRecordedDate} />
+          <CalorieInputError errors={validateErrors} column="recorded_at" />
         </div>
       </div>
-
-      <CalorieSubmit onClick={createManualCalorie} />
-    </div>
+      <SubmitButton>食べ物に換算</SubmitButton>
+    </form>
   );
 };
--- frontend/src/pages/CalorieInput.jsx
+++ frontend/src/pages/CalorieInput.jsx
@@ -8,7 +8,10 @@ export const CalorieInput = () => {
           <h2 className="mb-2 text-text text-5xl font-bold gradient-marker">消費したカロリーを入力！</h2>
           <p className="text-text text-base font-bold">運動で消費したカロリーを食べ物の個数に換算できます！</p>
         </div>
-        <ManualCalorieForm />
+        <div className="relative max-w-lg mx-auto pt-12 pb-10 border-slate-900 border-4 rounded-md before:content-[''] before:absolute before:w-56 before:h-10 before:bg-background before:inline-block before:-top-5 before:left-1/2 before:-translate-x-1/2">
+          <p className="absolute -top-7 left-1/2 -translate-x-1/2 w-52 mx-auto mb-5 py-3 rounded-full bg-text text-white font-bold text-lg">直接入力する</p>
+          <ManualCalorieForm />
+        </div>
       </div>
     </main>
   );
