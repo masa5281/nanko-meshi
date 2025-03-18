29f5417bad23e0dfb073b92bc704d9ab78b9b388
masa5281 <masa.h.5281@gmail.com>
2025-03-18 16:22:37 +0900

    feat: 登録済み食品一覧画面をTailwind CSSでスタイリング（#12）

--- frontend/src/components/RegisterdFoodList/RegisterdFoodItem.jsx
+++ frontend/src/components/RegisterdFoodList/RegisterdFoodItem.jsx
@@ -19,21 +19,21 @@ export const RegisterdFoodItem = () => {
   console.log(foodList);
 
   return (
-    <>
-      <h2 className="">登録した食品</h2>
-      <ul>
+    <div className="max-w-7xl mx-auto text-center">
+      <h2 className="inline-block mb-8 px-5 py-3 bg-black rounded-full text-white text-4xl">登録した食品</h2>
+      <ul className="grid grid-cols-3 gap-14 px-20">
         {foodList.map((food, index) => {
           return (
-            <li key={index}>
-              <div>
-                <img src={food.food_image.thumb.url} alt="" />
+            <li key={index} className="flex flex-col justify-center px-8 py-4 bg-white rounded-lg shadow-sm shadow-shadow">
+              <div className="mb-3 border-b-2">
+                <img src={food.food_image.thumb.url} alt="" className="mx-auto mb-3 rounded-lg" />
               </div>
-              <p>{food.name}</p>
-              <p>{food.calorie}</p>
+              <p className="inline-block mb-3 bg-amber-500 rounded-lg text-white text-lg">{food.name}</p>
+              <p className="text-lg"><span className="text-3xl">{food.calorie}</span>kcal</p>
             </li>
           )
         })}
       </ul>
-    </>
+    </div>
   );
 };
