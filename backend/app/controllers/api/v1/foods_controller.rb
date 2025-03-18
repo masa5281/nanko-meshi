bcb9d3ab300f164bc6e1036333d27af314f42ceb
masa5281 <masa.h.5281@gmail.com>
2025-03-19 08:22:54 +0900

    feat: Foodsコントローラのupdateアクションを定義（#12）

--- backend/app/controllers/api/v1/foods_controller.rb
+++ backend/app/controllers/api/v1/foods_controller.rb
@@ -13,6 +13,15 @@ class Api::V1::FoodsController < ApplicationController
     end
   end
 
+  def update
+    food = Food.find(params[:id])
+    if food.update(food_params)
+      render json: food
+    else
+      render json: food.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def food_params
