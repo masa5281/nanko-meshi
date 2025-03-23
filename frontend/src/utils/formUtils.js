ed9a5abd472449bffe3e0965d46d557cda672de0
masa5281 <masa.h.5281@gmail.com>
2025-03-23 17:00:08 +0900

    rafactor: useFormUtilsを整理（#14）

---
+++ frontend/src/utils/formUtils.js
@@ -0,0 +1,15 @@
+// 入力値を全角→半角変換
+export const onChangeToText = (e) => {
+  const inputText = e.target.value;
+  const toHalfWidth = inputText.replace(/[０-９]/g, (str) => String.fromCharCode(str.charCodeAt(0) - 0xFEE0));
+  return toHalfWidth;
+};
+
+// フォームの体重リスト
+export const setWeight = () => {
+  const weights = [];
+  for (let i = 20; i <= 150; i++) {
+    weights.push(i);
+  }
+  return weights;
+};
