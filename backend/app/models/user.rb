ca2ff1673e916d404c3e153bf850c3f2e0244654
masa5281 <masa.h.5281@gmail.com>
2025-03-25 17:34:56 +0900

    feat: Userモデルのバリデーションを追加し、エラーメッセージを翻訳（#14）

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :firebase_uid, presence: true, uniqueness: true
   validates :name, presence:, uniqueness: true, length: { maximum:20 }
+  validates :weight, numericality: { only_integer: true, greater_than: 19, less_than: 151 }, allow_nil: true
 end
