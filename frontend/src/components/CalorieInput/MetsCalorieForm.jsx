979eb97130a37257529709ff1fbc09f6e607c2aa
masa5281 <masa.h.5281@gmail.com>
2025-03-26 08:51:07 +0900

    feat: 運動時間を手動入力するフォームを実装（#14）

--- frontend/src/components/CalorieInput/MetsCalorieForm.jsx
+++ frontend/src/components/CalorieInput/MetsCalorieForm.jsx
@@ -1,15 +1,21 @@
-import { FormProvider, useForm } from "react-hook-form";
+// コンポーネント
 import { SubmitButton } from "../Button/SubmitButton";
-import { useEffect, useState } from "react";
-import { axiosClient } from "../../config/axiosClient";
+import { InputField } from "../InputField/InputField";
+// モジュール
 import { API_ENDPOINTS } from "../../utils/constants";
+import { axiosClient } from "../../config/axiosClient";
+// ライブラリ
+import { FormProvider, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+// アイコン
+import { MdAccessTimeFilled } from "react-icons/md";
 
 export const MetsCalorieForm = () => {
   const methods = useForm({
     mode: "onBlur",
     criteriaMode: "all"
   });
-  const { handleSubmit} = methods;
+  const { handleSubmit } = methods;
   const [dbMetsData, setDbMetsData] = useState("");
 
   useEffect(() => {
@@ -33,6 +39,14 @@ export const MetsCalorieForm = () => {
             <option key={met.id} value={met.mets_value}>{met.activity_name}</option>
           )}
         </select>
+        <InputField
+          id="activeTime"
+          type="text"
+          placeholder="例：30"
+          fieldName="activeTime"
+          iconComponent={<MdAccessTimeFilled />}
+          labelName="運動時間（分）"
+        />
         <SubmitButton>食べ物に換算</SubmitButton>
       </form>
     </FormProvider>
