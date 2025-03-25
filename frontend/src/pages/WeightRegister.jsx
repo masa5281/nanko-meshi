b51b041f45a90bdef2e2741064591ed521260b88
masa5281 <masa.h.5281@gmail.com>
2025-03-25 16:30:33 +0900

    feat: 体重選択フォームをTailwind CSSでスタイリング（#14）
    - フロント側のバリデーションをuseFormで設定

--- frontend/src/pages/WeightRegister.jsx
+++ frontend/src/pages/WeightRegister.jsx
@@ -1,31 +1,41 @@
 // コンポーネント
 import { AuthSubmitButton } from "../components/Button/AuthSubmitButton";
+import { IconWrapper } from "../components/IconWrapper";
 // モジュール
 import { setWeight } from "../utils/formUtils";
 import { useAuth } from "../context/AuthContext";
 import { getUserApi, updateUserApi } from "../api/userApi";
 import { ROUTES } from "../utils/constants";
 // ライブラリ
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FormProvider, useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 // カスタムフック
 import { useUserDataContext } from "../context/UserDataContext";
+// アイコン
+import { FaWeightScale } from "react-icons/fa6";
+import { GiWeightScale } from "react-icons/gi";
 
 export const WeightRegister = () => {
-  const [bodyWeight, setBodyWeight] = useState();
-  const bodyWeightRef = useRef(null);
   const weights = setWeight();
   const { user } = useAuth();
   const navigate = useNavigate();
   const { setDbUserData } = useUserDataContext();
+  const methods = useForm({
+    mode: "onBlur",
+    criteriaMode: "all"
+  });
+  const { register, watch, handleSubmit, formState: { errors } } = methods;
+  const [isTextPlaceholder, setIsTextPlaceholder] = useState(true);
+  const selectWeight = watch("userWeight");
 
-  const handleUpdateUser = async (e) => {
-    e.preventDefault();
+  const handleUpdateUser = async () => {
     try {
       const userData = await getUserApi(user.uid);
       await updateUserApi(
         userData.name,
-        bodyWeight,
+        selectWeight,
         userData.avatar,
         user.uid
       );
@@ -37,23 +47,58 @@ export const WeightRegister = () => {
     }
   };
 
+  // 体重選択のプレースホルダーを制御
+  useEffect(() => {
+    const selectPlaceholder = () => {
+      if (selectWeight === "") {
+        setIsTextPlaceholder(true);
+      } else {
+        setIsTextPlaceholder(false);
+      }
+    }
+    selectPlaceholder();
+  }, [selectWeight])
+
   return (
-    <div className="pt-12">
-      <div className="max-w-md mx-auto px-10 py-6 bg-header rounded-md shadow-md shadow-shadow">
-        <h2 className="mb-5 pb-2 border-b border-white text-white text-3xl text-center">現在の体重を選択</h2>
-        <form onSubmit={handleUpdateUser} className="flex flex-col">
-          <select
-            ref={bodyWeightRef}
-            value={bodyWeight}
-            onChange={(e) => setBodyWeight(e.target.value)}
-            className="mb-4"
-          >
-            <option value={""}>体重を選択してください</option>
-            {weights.map((weight) => <option key={weight} value={weight}>{weight}</option>)}
-          </select>
-          <AuthSubmitButton>決定</AuthSubmitButton>
-        </form>
+    <FormProvider {...methods}>
+      <div className="pt-12">
+        <div className="max-w-xs mx-auto px-10 py-6 bg-header rounded-md shadow-md shadow-shadow">
+          <h2 className="mb-5 pb-2 border-b border-white text-white text-3xl text-center">現在の体重を選択</h2>
+
+          <form onSubmit={handleSubmit(handleUpdateUser)} className="flex flex-col">
+            <div className="relative mb-4">
+              <label htmlFor="userWeight" className="absolute top-1 left-1 p-1 rounded-full bg-black">
+                <IconWrapper size={24} color="#fff">
+                  <GiWeightScale />
+                </IconWrapper>
+              </label>
+              <select
+                id="userWeight"
+                defaultValue=""
+                className={`${isTextPlaceholder ? "placeholder" : ""} w-full py-2 border-none rounded-full indent-8 focus:ring-2 focus:ring-secondary focus:border-secondary`}
+                {...register("userWeight", {
+                  required: "体重を選択してください"
+                })}
+              >
+                <option value="" disabled>体重を選択</option>
+                {weights.map((weight) => <option key={weight} value={weight}>{weight}</option>)}
+              </select>
+              <span className="absolute top-1.5 right-8 pointer-events-none">kg</span>
+              <ErrorMessage
+                errors={errors}
+                name="userWeight"
+                render={({ message }) =>
+                  message ? (
+                    <p className="pl-2 text-errorYellow text-sm">{message}</p>
+                  ) : null
+                }
+              />
+            </div>
+
+            <AuthSubmitButton>決定</AuthSubmitButton>
+          </form>
+        </div>
       </div>
-    </div>
+    </FormProvider>
   );
 };
