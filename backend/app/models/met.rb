e105e17322b965b1c8b2fa94681776052a3a5ba7
masa5281 <masa.h.5281@gmail.com>
2025-03-26 06:49:14 +0900

    feat: MetモデルとMetsテーブルの作成（#14）
    - activity_nameカラムにNotNull制約とunique制約を追加
    - mets_valueカラムにNotNull制約を追加

---
+++ backend/app/models/met.rb
@@ -0,0 +1,2 @@
+class Met < ApplicationRecord
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_25_074400) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_25_213243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_25_074400) do
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
     t.check_constraint "calorie > 0 AND calorie < 10000", name: "calorie_check"
+  end
+
+  create_table "mets", force: :cascade do |t|
+    t.string "activity_name", null: false
+    t.float "mets_value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_name"], name: "index_mets_on_activity_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
