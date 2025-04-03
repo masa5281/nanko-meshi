87f19750bcdc8cc6456950185ef5bcbd503d0b6a
masa5281 <masa.h.5281@gmail.com>
2025-04-03 15:25:05 +0900

    feat: Usersコントローラにdestroyアクションとルーティングを定義（#16）
    - set_userプライベートメソッドでユーザー取得処理を共通化

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
   skip_before_action :authenticate_request, only: %i[create]
 
   def show
-    user = User.find_by(firebase_uid: params[:firebase_uid])
-    render json: user.as_json(only: [:id, :name, :avatar, :weight])
+    render json: @user.as_json(only: [:id, :name, :avatar, :weight])
   end
 
   def create
@@ -23,9 +23,18 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      render json: @user.as_json(only: [:id, :weight])
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
     user = User.find_by(firebase_uid: params[:firebase_uid])
-    if user.update(user_params)
-      render json: user.as_json(only: [:id, :weight])
+    if user
+      user.destroy
+      head :no_content
     else
       render json: user.errors, status: :unprocessable_entity
     end
@@ -35,5 +44,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:firebase_uid, :name, :avatar, :weight)
+  end
+
+  def set_user
+    @user = User.find_by(firebase_uid: params[:firebase_uid])
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, param: :firebase_uid, only: %i[show create update]
+      resources :users, param: :firebase_uid, only: %i[show create update destroy]
       resources :foods, only: %i[index create update destroy]
       resources :calories, only: %i[create]
       resources :mets, only: %i[index]
