9eebd43fce7d362838a158d2f414bea9f67ab505
masa5281 <masa.h.5281@gmail.com>
2025-04-13 14:51:56 +0900

    feat: Caloriesコントローラのindexアクションとルーティングを定義（#18）

--- backend/app/controllers/api/v1/calories_controller.rb
+++ backend/app/controllers/api/v1/calories_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::CaloriesController < ApplicationController
+  def index
+    calories = current_user.calories
+    render json: calories.as_json(only: [:id, :burned_calorie, :recorded_at])
+  end
+
   def create
     calorie = @current_user.calories.new(calorie_params)
     if calorie.save
