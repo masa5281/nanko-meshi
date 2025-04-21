94b9ae6b058ab11bb3723ef551b1d77998581255
masa5281 <masa.h.5281@gmail.com>
2025-04-22 07:26:20 +0900

    feat: 【月グラフ】月グラフを実装（#18）

---
+++ frontend/src/components/BarGraph/MonthGraph.jsx
@@ -0,0 +1,180 @@
+import { useEffect, useMemo, useState } from 'react';
+import {
+  BarChart,
+  Bar,
+  Legend,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  Cell,
+} from 'recharts';
+import { useCalorieApi } from '../../hooks/useCalorieApi';
+
+export const MonthGraph = () => {
+  const { calorieList } = useCalorieApi();
+  const [selectMonth, setSelectMonth] = useState(new Date());
+  const [selectCalorie, setSelectCalorie] = useState("");
+  const [barData, setBarData] = useState([]);
+  const month = useMemo(() => ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"], []);
+  const [isCurrentYear, setIsCurrentYear] = useState(true);
+
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth();
+  const currentDate = today.getDate();
+  const [startYearDate, setStartYearDate] = useState(new Date(currentYear, 0, 1));
+
+  const CustomizedTick = ({ x, y, payload }) => (
+    <g transform={`translate(${x},${y})`}>
+      <rect x={-17.5} y={-5} width={35} height={35} fill={selectColor(payload, "#FF3838", "#f0f0f0")} rx={100} ry={100} />
+      <text x={0} y={13} dy={5} textAnchor="middle" fontSize={14} fill={selectColor(payload, "#fff", "#333")}>
+        {payload.value}
+      </text>
+    </g>
+  );
+
+  const selectColor = (payload, truthyColor, falsyColor) => {
+    return (selectMonth.getMonth() === payload.index) ? truthyColor : falsyColor;
+  };
+
+  // グラフ表示に合わせてフォーマット
+  const formatGraphMonth = (date, index = 0) => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + index);
+    return `${newDate.getFullYear()}年${newDate.getMonth() + 1}月`;
+  };
+
+  // 何月分足すかを処理
+  const onClickGraph = (data, index) => {
+    const targetMonth = new Date(startYearDate);
+    targetMonth.setMonth(targetMonth.getMonth() + index);
+    setSelectMonth(targetMonth);
+    setSelectCalorie(data.burnedCalorie);
+  };
+
+  useEffect(() => {
+    const endYearDate = new Date(startYearDate.getFullYear(), 11, 31);
+    const formatToday = new Date(currentYear, currentMonth, currentDate);
+    setIsCurrentYear(formatToday >= startYearDate && formatToday <= endYearDate);
+  }, [currentYear, currentMonth, currentDate, startYearDate]);
+
+  // DBのカロリーを該当の月のグラフに配置
+  useEffect(() => {
+    const data = [];
+    for (let i = 0; i < 12; i++) {
+      const baseDate = new Date(startYearDate);
+      baseDate.setMonth(baseDate.getMonth() + i);
+      const dateStr = `${baseDate.getFullYear()}-${String(baseDate.getMonth() + 1).padStart(2, "0")}`;
+      const newCalorieList = calorieList.filter(calorie => calorie.recorded_at.slice(0, 7) === dateStr);
+      const sumCalorie = newCalorieList.reduce((totalCalorie, object) => totalCalorie + object.burned_calorie, 0);
+      if (i === selectMonth.getMonth()) {
+        setSelectCalorie(sumCalorie);
+      }
+      data.push({
+        month: month[i],
+        burnedCalorie: sumCalorie
+      })
+    }
+    setBarData(data);
+  }, [startYearDate, calorieList, selectMonth, month]);
+
+  const customLegend = ({ payload }) => {
+    return (
+      <ul className='text-left'>
+        {
+          payload.map((entry, index) => (
+            <li key={`item-${index}`}>
+              <span className='inline-block w-3 h-3 mr-1 bg-primary rounded-md'></span>
+              {entry.value}
+            </li>
+          ))
+        }
+      </ul>
+    );
+  };
+
+  const onPrevYear = () => {
+    const prevYearDate = new Date(startYearDate.getFullYear() - 1, 0, 1);
+    setStartYearDate(prevYearDate);
+    setSelectMonth(prevYearDate);
+  };
+
+  const onNextYear = () => {
+    const nextYearDate = new Date(startYearDate.getFullYear() + 1, 0, 1);
+    setStartYearDate(nextYearDate);
+    setSelectMonth(nextYearDate);
+  };
+
+  return (
+    <>
+      <p className='inline-block mb-2 border-b-2 border-black text-xl'>{formatGraphMonth(selectMonth)}</p>
+      <p className='text-primary text-5xl font-bold'>{selectCalorie}<span className='text-black text-xl'>kcal</span></p>
+
+      <ResponsiveContainer className={""} width="100%" height={350} >
+        <BarChart
+          data={barData}
+          barSize={50}
+          margin={{
+            top: 10,
+            right: 10,
+            left: -10,
+            bottom: 20,
+          }}
+        >
+          <XAxis
+            dataKey="month"
+            tickSize={0}
+            tickMargin={10}
+            tick={<CustomizedTick />}
+          />
+          <YAxis
+            ticks={[2000, 4000, 6000, 8000, 10000]}
+            tickSize={0}
+            tickMargin={10}
+            axisLine={false}
+          />
+          <Legend
+            content={customLegend}
+            payload={[
+              { value: "消費カロリー" }
+            ]}
+            wrapperStyle={{
+              marginLeft: "60px",
+              paddingTop: "25px",
+            }}
+          />
+          <CartesianGrid stroke="#d0d0d0" vertical={false} />
+          <Bar
+            dataKey="burnedCalorie"
+            fill="#FF6E2B"
+            radius={[5, 5, 0, 0]}
+            onClick={onClickGraph}
+          >
+            {barData.map((_, index) => (
+              <Cell
+                cursor="pointer"
+                key={index}
+              />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+
+      <div className='mr-3 text-lg text-right'>
+        <button
+          className="relative mr-4 before:content-[''] before:absolute before:top-2.5 before:-left-3 before:w-3 before:h-3 before:border-t-2 before:border-l-2 before:border-black before:-rotate-45"
+          onClick={onPrevYear}
+        >
+          前週
+        </button>
+        <button
+          className={`${isCurrentYear ? "text-gray-400 pointer-events-none before:border-gray-400" : null} relative mr-4 before:content-[''] before:absolute before:top-2.5 before:-right-3 before:w-3 before:h-3 before:border-t-2 before:border-l-2 before:border-black before:rotate-[135deg]`}
+          onClick={onNextYear}
+        >
+          翌週
+        </button>
+      </div>
+    </>
+  );
+};
