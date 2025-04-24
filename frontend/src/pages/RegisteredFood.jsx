ef5c0165da370bab20f4cb50485d76d710d31ab4
masa5281 <masa.h.5281@gmail.com>
2025-04-24 11:35:27 +0900

    feat: 登録した食品・みんなの食品の切り替えタブを実装（#20）

--- frontend/src/pages/RegisteredFood.jsx
+++ frontend/src/pages/RegisteredFood.jsx
@@ -1,24 +1,56 @@
 import { FormProvider, useForm } from "react-hook-form";
 import { MyRegisteredFood } from "../components/RegisterdFood/MyRegisteredFood";
 import { AllRegisteredFood } from "../components/RegisterdFood/AllRegisteredFood";
+import { motion } from "motion/react";
+import { useState } from "react";
 
 export const RegisteredFood = () => {
   const methods = useForm({
     mode: "onBlur",
     criteriaMode: "all"
   });
+  const [selectTab, setSelectTab] = useState("myFood");
+
+  // アニメーションをstateに応じて変化
+  const variants = {
+    myFood: { x: 0 },
+    allFood: { x: 155 },
+  };
 
   return (
     <FormProvider {...methods}>
       <div className="max-w-7xl mx-auto text-center">
-        <div className="relative flex gap-8 justify-center w-80 py-3 mb-8 mx-auto bg-white rounded-full text-xl">
-          <p>登録した食品</p>
-          <p>みんなの食品</p>
-          <span className="absolute w-20 h-8 bg-primary"></span>
+        <div className="relative w-80 py-3 mb-8 mx-auto bg-white rounded-full text-xl shadow-sm shadow-shadow">
+          <div className=" flex gap-8 justify-center">
+            <p
+              className={`${selectTab === "myFood" ? "text-white" : "text-black"} z-10 hover:cursor-pointer`}
+              onClick={() => setSelectTab("myFood")}
+            >
+              登録した食品
+            </p>
+            <p
+              className={`${selectTab === "allFood" ? "text-white" : "text-black"} z-10 hover:cursor-pointer`}
+              onClick={() => setSelectTab("allFood")}
+            >
+              みんなの食品
+            </p>
+          </div>
+          <motion.span
+            className="absolute top-1.5 left-2 w-[150px] h-10 bg-primary rounded-full"
+            initial={false}
+            animate={selectTab}
+            variants={variants}
+            transition={{ type: "tween" }}
+          >
+          </motion.span>
         </div>
 
-        <MyRegisteredFood />
-        <AllRegisteredFood />
+        {selectTab === "myFood" && (
+          <MyRegisteredFood />
+        )}
+        {selectTab === "allFood" && (
+          <AllRegisteredFood />
+        )}
       </div>
     </FormProvider>
   );
