04893adadd9cc2da43129f0a049941408987f3a1
masa5281 <masa.h.5281@gmail.com>
2025-04-25 19:13:02 +0900

    feat: お気に入りと食品取得のルーティングを定義（#20）

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, param: :firebase_uid, only: %i[show create update destroy]
-      resources :foods, only: %i[index create update destroy]
+      resources :users, param: :firebase_uid, only: %i[show create update destroy] do
+        get "favorites", on: :collection
+      end
+      resources :foods, only: %i[index create update destroy] do
+        resources :favorites, only: %i[create destroy]
+        get "other", on: :collection
+      end
       resources :calories, only: %i[index create]
       resources :mets, only: %i[index]
     end
