11dbab5dcc1a0a330c0735ac9310adc77aadecb6
masa5281 <masa.h.5281@gmail.com>
2025-04-27 19:17:14 +0900

    feat: Usersコントローラのindexアクションとルーティングを定義（#20）

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, param: :firebase_uid, only: %i[show create update destroy] do
+      resources :users, param: :firebase_uid, only: %i[index show create update destroy] do
         get "favorites", on: :collection
       end
       resources :foods, only: %i[index create update destroy] do
