2c1cd22a365b68582b1c3c2bbfa1370c70b67004
masa5281 <masa.h.5281@gmail.com>
2025-05-05 08:34:06 +0900

    feat: Xシェア機能の実装（#24）

--- frontend/src/components/FoodConversion/FoodMobileItem.jsx
+++ frontend/src/components/FoodConversion/FoodMobileItem.jsx
@@ -1,39 +1,42 @@
+import { XShareButton } from "../Button/XShareButton";
 import { AnimateProgress } from "./AnimateProgress";
 
 export const FoodMobileItem = ({
   food,
-  calorieToFoodCount,
+  foodCount,
   foodPercentage,
   className,
 }) => {
   return (
-    <li className={`${className} p-4 bg-white rounded-lg shadow-sm shadow-shadow`}>
+    <li className={`${className} relative p-4 bg-white rounded-lg shadow-sm shadow-shadow`}>
+      <XShareButton food={food} foodCount={foodCount} />
+
       <div className="flex mb-3">
         <div className="max-w-24 max-h-24 mr-2 rounded-md overflow-hidden">
           <img src={food.food_image.thumb.url} alt="食品画像" className="w-full h-full" />
         </div>
         <div>
-          <p className={`${food.name.length >= 8 ? "text-sm" : "text-base"} mb-1 font-bold`}>{food.name}</p>
+          <p className={`${food.name.length >= 8 ? "text-sm" : "text-base"} max-w-[150px] mb-1 font-bold`}>{food.name}</p>
           <p><span className="text-xl">{food.calorie}</span>kcal</p>
         </div>
       </div>
-
       <div>
         <div className="flex justify-between items-end mb-2">
           {
             foodPercentage(food.calorie) === 100 ? (
-              <p className="text-lg md:text-2xl font-bold text-center">
-                <span className="text-2xl md:text-4xl text-redBar">{foodPercentage(food.calorie)}%</span>
+              <p className="text-lg font-bold text-center">
+                <span className="text-3xl text-redBar">{foodPercentage(food.calorie)}%</span>
                 以上消費
               </p>
             ) : (
-              <p className="text-lg md:text-2xl font-bold text-center">
-                <span className="text-2xl md:text-4xl">{foodPercentage(food.calorie)}%</span>
+              <p className="text-lg font-bold text-center">
+                <span className="text-3xl">{foodPercentage(food.calorie)}%</span>
                 を消費
               </p>
             )
           }
-          <p className="max-w-32 p-1 bg-secondary rounded-lg text-white text-4xl font-bold">{calorieToFoodCount(food.calorie)}
+          <p className="max-w-32 p-1 bg-secondary rounded-lg text-white text-4xl font-bold">
+            {foodCount}
             <span className="text-2xl">個分</span>
           </p>
         </div>
