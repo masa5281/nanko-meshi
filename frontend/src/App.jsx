ec1ab567c22fd98d18f17f8096ab5431c8ad68e6
masa5281 <masa.h.5281@gmail.com>
2025-05-10 20:41:50 +0900

    feat: フッターを常に表示（#26）

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import { BarGraph } from "./pages/BarGraph";
 import { TopPage } from "./pages/TopPage";
 import { Terms } from "./pages/Terms";
 import { Privacy } from "./pages/Privacy";
+import { Footer } from "./components/Footer";
 
 import { ProtectedRoute } from "./Routes/ProtectedRoute";
 import { GuestRoute } from "./Routes/GuestRoute";
@@ -49,136 +50,139 @@ export const App = () => {
               {stateError === 404 ? (
                 <Page404 />
               ) : (
-                <div>
+                <div className="flex flex-col min-h-screen">
                   <Header />
-                  <Routes>
-                    <Route
-                      path="*"
-                      element={
-                        <Page404 />
-                      }
-                    />
-                    <Route
-                      path={ROUTES.TOP}
-                      element={
-                        <TopPage />
-                      }
-                    />
-                    <Route
-                      path={ROUTES.TERMS}
-                      element={
-                        <Terms />
-                      }
-                    />
-                    <Route
-                      path={ROUTES.PRIVACY}
-                      element={
-                        <Privacy />
-                      }
-                    />
-                    <Route
-                      path={ROUTES.AUTH.SIGN_UP}
-                      element={
-                        <GuestRoute>
-                          <SignUp />
-                        </GuestRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.AUTH.SIGN_IN}
-                      element={
-                        <GuestRoute>
-                          <SignIn />
-                        </GuestRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.AUTH.PASSWORD_RESET}
-                      element={
-                        <GuestRoute>
-                          <PasswordResetForm />
-                        </GuestRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.AUTH.PASSWORD_SENT}
-                      element={
-                        <GuestRoute>
-                          <PasswordResetSuccess />
-                        </GuestRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.AUTH.WEIGHT}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator skipWeightCheck={true}>
-                            <WeightRegister />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.USERS.PROFILE}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <Profile />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.FOODS.CONVERSION}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <FoodConversion />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.FOODS.REGISTER}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <FoodRegister />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.FOODS.MY_REGISTERED}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <RegisteredFood />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.CALORIE.INPUT}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <CalorieInput />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path={ROUTES.CALORIE.GRAPH}
-                      element={
-                        <ProtectedRoute>
-                          <WeightCheckNavigator>
-                            <BarGraph />
-                          </WeightCheckNavigator>
-                        </ProtectedRoute>
-                      }
-                    />
-                  </Routes>
+                  <main className="flex-1">
+                    <Routes>
+                      <Route
+                        path="*"
+                        element={
+                          <Page404 />
+                        }
+                      />
+                      <Route
+                        path={ROUTES.TOP}
+                        element={
+                          <TopPage />
+                        }
+                      />
+                      <Route
+                        path={ROUTES.TERMS}
+                        element={
+                          <Terms />
+                        }
+                      />
+                      <Route
+                        path={ROUTES.PRIVACY}
+                        element={
+                          <Privacy />
+                        }
+                      />
+                      <Route
+                        path={ROUTES.AUTH.SIGN_UP}
+                        element={
+                          <GuestRoute>
+                            <SignUp />
+                          </GuestRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.AUTH.SIGN_IN}
+                        element={
+                          <GuestRoute>
+                            <SignIn />
+                          </GuestRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.AUTH.PASSWORD_RESET}
+                        element={
+                          <GuestRoute>
+                            <PasswordResetForm />
+                          </GuestRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.AUTH.PASSWORD_SENT}
+                        element={
+                          <GuestRoute>
+                            <PasswordResetSuccess />
+                          </GuestRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.AUTH.WEIGHT}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator skipWeightCheck={true}>
+                              <WeightRegister />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.USERS.PROFILE}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <Profile />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.FOODS.CONVERSION}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <FoodConversion />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.FOODS.REGISTER}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <FoodRegister />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.FOODS.MY_REGISTERED}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <RegisteredFood />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.CALORIE.INPUT}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <CalorieInput />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path={ROUTES.CALORIE.GRAPH}
+                        element={
+                          <ProtectedRoute>
+                            <WeightCheckNavigator>
+                              <BarGraph />
+                            </WeightCheckNavigator>
+                          </ProtectedRoute>
+                        }
+                      />
+                    </Routes>
+                  </main>
+                  <Footer />
                 </div>
               )}
             </Router>
