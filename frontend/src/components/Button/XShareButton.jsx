de1821c468a6b708ad68caebe3ebb817c2428b7b
masa5281 <masa.h.5281@gmail.com>
2025-06-05 18:22:03 +0900

    update: 環境変数名の変更

--- frontend/src/components/Button/XShareButton.jsx
+++ frontend/src/components/Button/XShareButton.jsx
@@ -4,7 +4,7 @@ import { TwitterShareButton, XIcon } from "react-share";
 export const XShareButton = ({ food, foodCount }) => {
   return (
     <TwitterShareButton
-      url={`${process.env.REACT_APP_DEV_URL}${ROUTES.AUTH.SIGN_IN}`}
+      url={`${process.env.REACT_APP_BASE_URL}${ROUTES.AUTH.SIGN_IN}`}
       title={`今日の運動成果：${food.name} ${foodCount}個分のカロリーを消費！🔥\n#なんこメシ #ダイエット`}
       className="absolute top-3 right-3"
     >
