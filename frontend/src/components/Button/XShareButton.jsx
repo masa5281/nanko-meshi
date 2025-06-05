1ede49a4e81d81ab873bf5fd81dcb3e724a65d92
masa5281 <masa.h.5281@gmail.com>
2025-06-06 06:51:26 +0900

    chore: Xシェア用metaタグの内容変更

--- frontend/src/components/Button/XShareButton.jsx
+++ frontend/src/components/Button/XShareButton.jsx
@@ -4,8 +4,8 @@ import { TwitterShareButton, XIcon } from "react-share";
 export const XShareButton = ({ food, foodCount }) => {
   return (
     <TwitterShareButton
-      url={`${process.env.REACT_APP_BASE_URL}${ROUTES.AUTH.SIGN_IN}`}
-      title={`今日の運動成果：${food.name} ${foodCount}個分のカロリーを消費！🔥\n#なんこメシ #ダイエット`}
+      url={`${process.env.REACT_APP_BASE_URL}${ROUTES.TOP}`}
+      title={`【今日の運動成果】\n${food.name} ${foodCount}個分のカロリーを消費！🔥\n\n#なんこメシ\n#ダイエット`}
       className="absolute top-3 right-3"
     >
       <XIcon size={20} round={true} />
